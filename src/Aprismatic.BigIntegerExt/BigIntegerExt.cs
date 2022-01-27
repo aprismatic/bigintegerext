e6cfbf8a580b9bb6fdd0a339de4ba7217ff56704
bazzilic <bazzilic@gmail.com>
2022-01-27 17:22:34 +0800

    testing 2k, 50k, 100k, 200k

--- src/Aprismatic.BigIntegerExt/BigIntegerExt.cs
+++ src/Aprismatic.BigIntegerExt/BigIntegerExt.cs
@@ -15,30 +15,8 @@ using System.Security.Cryptography;
 
 namespace Aprismatic
 {
-    public static class BigIntegerExt
+    public static partial class BigIntegerExt
     {
-        // primes smaller than 2000 to test the generated prime number
-        public static readonly ulong[] PrimesBelow2000 = {
-           2,    3,    5,    7,   11,   13,   17,   19,   23,   29,   31,   37,   41,   43,   47,   53,   59,   61,   67,   71,
-          73,   79,   83,   89,   97,  101,  103,  107,  109,  113,  127,  131,  137,  139,  149,  151,  157,  163,  167,  173,
-         179,  181,  191,  193,  197,  199,  211,  223,  227,  229,  233,  239,  241,  251,  257,  263,  269,  271,  277,  281,
-         283,  293,  307,  311,  313,  317,  331,  337,  347,  349,  353,  359,  367,  373,  379,  383,  389,  397,  401,  409,
-         419,  421,  431,  433,  439,  443,  449,  457,  461,  463,  467,  479,  487,  491,  499,  503,  509,  521,  523,  541,
-         547,  557,  563,  569,  571,  577,  587,  593,  599,  601,  607,  613,  617,  619,  631,  641,  643,  647,  653,  659,
-         661,  673,  677,  683,  691,  701,  709,  719,  727,  733,  739,  743,  751,  757,  761,  769,  773,  787,  797,  809,
-         811,  821,  823,  827,  829,  839,  853,  857,  859,  863,  877,  881,  883,  887,  907,  911,  919,  929,  937,  941,
-         947,  953,  967,  971,  977,  983,  991,  997, 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069,
-        1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223,
-        1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291, 1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373,
-        1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511,
-        1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657,
-        1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811,
-        1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987,
-        1993, 1997, 1999 };
-
-        public static readonly BigInteger[] PrimesBelow2000_BI = PrimesBelow2000.Select(p => (BigInteger)p).ToArray();
-
-
         /// <summary>
         /// Calculates the modulo inverse of `this`.
         /// </summary>
@@ -199,6 +177,51 @@ namespace Aprismatic
             return result;
         }
 
+        public static BigInteger GenPseudoPrime100k(this BigInteger T, int bits, int confidence, RandomNumberGenerator rng)
+        {
+            if (bits < 2) throw new ArgumentOutOfRangeException(nameof(bits), bits, "GenPseudoPrime can only generate prime numbers of 2 bits or more");
+
+            BigInteger result;
+
+            do
+            {
+                result = BigInteger.Zero.GenRandomBits(bits, rng);
+                result |= BigInteger.One; // make it odd
+            } while (!result.IsProbablePrime100k(confidence, rng));
+
+            return result;
+        }
+
+        public static BigInteger GenPseudoPrime500k(this BigInteger T, int bits, int confidence, RandomNumberGenerator rng)
+        {
+            if (bits < 2) throw new ArgumentOutOfRangeException(nameof(bits), bits, "GenPseudoPrime can only generate prime numbers of 2 bits or more");
+
+            BigInteger result;
+
+            do
+            {
+                result = BigInteger.Zero.GenRandomBits(bits, rng);
+                result |= BigInteger.One; // make it odd
+            } while (!result.IsProbablePrime50k(confidence, rng));
+
+            return result;
+        }
+
+        public static BigInteger GenPseudoPrime1M(this BigInteger T, int bits, int confidence, RandomNumberGenerator rng)
+        {
+            if (bits < 2) throw new ArgumentOutOfRangeException(nameof(bits), bits, "GenPseudoPrime can only generate prime numbers of 2 bits or more");
+
+            BigInteger result;
+
+            do
+            {
+                result = BigInteger.Zero.GenRandomBits(bits, rng);
+                result |= BigInteger.One; // make it odd
+            } while (!result.IsProbablePrime200k(confidence, rng));
+
+            return result;
+        }
+
 
         /// <summary>
         /// Generates a random probable safe prime positive BigInteger of exactly the specified bit length using the provided RNG.
@@ -314,6 +337,96 @@ namespace Aprismatic
             else
             {
                 foreach (var smallPrime in PrimesBelow2000_BI)
+                {
+                    if ((thisVal % smallPrime).IsZero)
+                        return false;
+                }
+            }
+
+            return thisVal.RabinMillerTest(confidence, rng);
+        }
+
+        public static bool IsProbablePrime100k(this BigInteger T, int confidence, RandomNumberGenerator rng)
+        {
+            var thisVal = BigInteger.Abs(T);
+            if (thisVal.IsZero || thisVal.IsOne) return false;
+
+            if (thisVal <= UInt64.MaxValue)
+            {
+                var uival = (UInt64) thisVal;
+
+                foreach (var smallPrime in PrimesBelow100k)
+                {
+                    if (smallPrime >= uival)
+                        return true;
+
+                    if (uival % smallPrime == 0)
+                        return false;
+                }
+            }
+            else
+            {
+                foreach (var smallPrime in PrimesBelow100k_BI)
+                {
+                    if ((thisVal % smallPrime).IsZero)
+                        return false;
+                }
+            }
+
+            return thisVal.RabinMillerTest(confidence, rng);
+        }
+
+        public static bool IsProbablePrime50k(this BigInteger T, int confidence, RandomNumberGenerator rng)
+        {
+            var thisVal = BigInteger.Abs(T);
+            if (thisVal.IsZero || thisVal.IsOne) return false;
+
+            if (thisVal <= UInt64.MaxValue)
+            {
+                var uival = (UInt64) thisVal;
+
+                foreach (var smallPrime in PrimesBelow50k)
+                {
+                    if (smallPrime >= uival)
+                        return true;
+
+                    if (uival % smallPrime == 0)
+                        return false;
+                }
+            }
+            else
+            {
+                foreach (var smallPrime in PrimesBelow50k_BI)
+                {
+                    if ((thisVal % smallPrime).IsZero)
+                        return false;
+                }
+            }
+
+            return thisVal.RabinMillerTest(confidence, rng);
+        }
+
+        public static bool IsProbablePrime200k(this BigInteger T, int confidence, RandomNumberGenerator rng)
+        {
+            var thisVal = BigInteger.Abs(T);
+            if (thisVal.IsZero || thisVal.IsOne) return false;
+
+            if (thisVal <= UInt64.MaxValue)
+            {
+                var uival = (UInt64) thisVal;
+
+                foreach (var smallPrime in PrimesBelow200k)
+                {
+                    if (smallPrime >= uival)
+                        return true;
+
+                    if (uival % smallPrime == 0)
+                        return false;
+                }
+            }
+            else
+            {
+                foreach (var smallPrime in PrimesBelow200k_BI)
                 {
                     if ((thisVal % smallPrime).IsZero)
                         return false;
---
+++ test/Perf/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography;
+using Aprismatic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+
+namespace MyBenchmark
+{
+    [SimpleJob(runStrategy: RunStrategy.Throughput, invocationCount: 5000)]
+    public class Bench
+    {
+        private readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
+
+        [Params(4,8,16)]
+        public int confidence;
+
+        [Params(256,512,1024,2048,4096)]
+        public int bits;
+
+        private BigInteger[] numbers;
+        private const int amount = 10000;
+        private int index;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            numbers = new BigInteger[amount];
+            for (var i = 0; i < amount; i++)
+            {
+                numbers[i] = BigInteger.One.GenRandomBits(bits, rng);
+                numbers[i] |= BigInteger.One;
+            }
+        }
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            index = 0;
+        }
+
+        [Benchmark]
+        public bool b2k() => numbers[index++].IsProbablePrime(confidence, rng);
+
+        [Benchmark]
+        public bool b50k() => numbers[index++].IsProbablePrime50k(confidence, rng);
+
+        [Benchmark]
+        public bool b100k() => numbers[index++].IsProbablePrime100k(confidence, rng);
+
+        [Benchmark]
+        public bool b200k() => numbers[index++].IsProbablePrime200k(confidence, rng);
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Bench>();
+        }
+    }
+}
