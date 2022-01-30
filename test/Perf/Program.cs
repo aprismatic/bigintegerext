0d7c9fc1e2c78869a4466b191433006d6a6442c8
bazzilic <bazzilic@gmail.com>
2022-01-30 16:27:53 +0800

    thorough testing

--- test/Perf/Program.cs
+++ test/Perf/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Immutable;
 using System.Numerics;
 using System.Security.Cryptography;
 using Aprismatic;
@@ -8,16 +9,19 @@ using BenchmarkDotNet.Running;
 
 namespace MyBenchmark
 {
-    [SimpleJob(runStrategy: RunStrategy.Throughput, invocationCount: 10000)]
+    [SimpleJob(runStrategy: RunStrategy.Throughput, invocationCount: 750)]
     public class Bench
     {
         private readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
 
-        [Params(4,8,12,16)]
+        [Params(4,10,16)]
         public int confidence;
 
-        [Params(256,512,1024,2048,4096)]
+        [Params(128,256,512,1024,2048,4096,8192)]
         public int bits;
+
+        [Params(1,2,5,10,20,30,40,50,75,100,125,150,200,250,300,350,400,500,600,700,800,900,1000)]
+        public ulong PrimeListCap;
 
         private BigInteger[] numbers;
         private const int amount = 10000;
@@ -37,20 +41,19 @@ namespace MyBenchmark
         [IterationSetup]
         public void IterationSetup()
         {
+            BigIntegerExt.PrimesBelow2000 = BigIntegerExt.PrimesBelow1M.Where(x => x < PrimeListCap * 1000).OrderBy(x => x).ToArray();
+            BigIntegerExt.PrimesBelow2000_BI = BigIntegerExt.PrimesBelow2000.Select(p => (BigInteger) p).ToArray();
+
+            if (BigIntegerExt.PrimesBelow2000[^1] >= PrimeListCap) throw new Exception("selection");
+            if (BigIntegerExt.PrimesBelow2000.Length != BigIntegerExt.PrimesBelow2000_BI.Length) throw new Exception("lengths");
+            for (var i = 0; i < BigIntegerExt.PrimesBelow2000.Length; i++)
+                if (BigIntegerExt.PrimesBelow2000[i] != BigIntegerExt.PrimesBelow2000_BI[i]) throw new Exception("values");
+
             index = 0;
         }
 
         [Benchmark]
-        public bool b2k() => numbers[index++].IsProbablePrime(confidence, rng);
-
-        [Benchmark]
-        public bool b50k() => numbers[index++].IsProbablePrime50k(confidence, rng);
-
-        [Benchmark]
-        public bool b100k() => numbers[index++].IsProbablePrime100k(confidence, rng);
-
-        [Benchmark]
-        public bool b200k() => numbers[index++].IsProbablePrime200k(confidence, rng);
+        public bool bench() => numbers[index++].IsProbablePrime(confidence, rng);
     }
 
     public static class Program
