c5ba0a701d19aa7076113484ecbac779aa3f1190
bazzilic <bazzilic@gmail.com>
2022-02-04 21:09:39 +0800

    aaa

--- test/Perf/Program.cs
+++ test/Perf/Program.cs
@@ -15,14 +15,11 @@ namespace MyBenchmark
     {
         private readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
 
-        [Params(4,/*10,*/16)]
-        public int confidence;
-
         [Params(128,256,512/*,1024,2048,4096,8192*/)]
-        public int bits;
+        public int bit;
 
         [Params(1,2,5,10,20/*,30,40,50,75,100,125,150,200,250,300,350,400,500,600,700,800,900,1000*/)]
-        public ulong PrimeListCap;
+        public ulong prims;
 
         private BigInteger[] numbers;
         private const int amount = 10000;
@@ -36,19 +33,22 @@ namespace MyBenchmark
             {
                 do
                 {
-                    numbers[i] = BigInteger.One.GenRandomBits(bits, rng);
-                    numbers[i] |= BigInteger.One;
-                } while (numbers[i] % 3 == 0 || numbers[i] % 5 == 0 || numbers[i] % 7 == 0 || numbers[i] % 11 == 0);
+                    numbers[i] = BigInteger.One.GenRandomBits(bit, rng);
+                } while (numbers[i] % 2 == 0 ||
+                         numbers[i] % 3 == 0 ||
+                         numbers[i] % 5 == 0 ||
+                         numbers[i] % 7 == 0 ||
+                         numbers[i] % 11 == 0);
             }
         }
 
         [IterationSetup]
         public void IterationSetup()
         {
-            BigIntegerExt.PrimesBelow2000 = BigIntegerExt.PrimesBelow1M.Where(x => x < PrimeListCap * 1000).OrderBy(x => x).ToArray();
+            BigIntegerExt.PrimesBelow2000 = BigIntegerExt.PrimesBelow1M.Where(x => x < prims * 1000).OrderBy(x => x).ToArray();
             BigIntegerExt.PrimesBelow2000_BI = BigIntegerExt.PrimesBelow2000.Select(p => (BigInteger) p).ToArray();
 
-            if (BigIntegerExt.PrimesBelow2000[^1] >= PrimeListCap*1000) throw new Exception("selection");
+            if (BigIntegerExt.PrimesBelow2000[^1] >= prims*1000) throw new Exception("selection");
             if (BigIntegerExt.PrimesBelow2000.Length != BigIntegerExt.PrimesBelow2000_BI.Length) throw new Exception("lengths");
             for (var i = 0; i < BigIntegerExt.PrimesBelow2000.Length; i++)
                 if (BigIntegerExt.PrimesBelow2000[i] != BigIntegerExt.PrimesBelow2000_BI[i]) throw new Exception("values");
@@ -57,7 +57,13 @@ namespace MyBenchmark
         }
 
         [Benchmark]
-        public bool bench() => numbers[index++].IsProbablePrime(confidence, rng);
+        public bool c4() => numbers[index++].IsProbablePrime(4, rng);
+
+        [Benchmark]
+        public bool c10() => numbers[index++].IsProbablePrime(10, rng);
+
+        [Benchmark]
+        public bool c16() => numbers[index++].IsProbablePrime(16, rng);
     }
 
     public static class Program
@@ -66,8 +72,9 @@ namespace MyBenchmark
         {
             BenchmarkRunner.Run<Bench>(
                 DefaultConfig.Instance
+                    .AddExporter(CsvMeasurementsExporter.Default)
                     .AddExporter(RPlotExporter.Default)
-                    .AddExporter(CsvMeasurementsExporter.Default));
+                    );
         }
     }
 }
